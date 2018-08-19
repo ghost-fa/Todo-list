8903227fb3901e5c2af116a53eeb0428f0eb2041
ghost-fk <fayyad_r22@yahoo.com>
2018-08-19 17:09:28 +0200

    add tagline in App component and add a prop to the Header component, that we are using inside. The tagline below our h1 element will be dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = props => {
   return (
-    <header className="header">
+    <header>
       <h1>What To Do!</h1>
-      <p className="tagline">This could be your bucket list.</p>
+      <p className="tagline">{props.tagline}</p>
     </header>
   );
 };
