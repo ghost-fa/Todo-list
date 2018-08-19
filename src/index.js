c8f54158faa42407aca069e5c1f4cc04dce93aab
ghost-fk <fayyad_r22@yahoo.com>
2018-08-19 19:22:16 +0200

    change color and margin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Router from './components/App';
 import './styles/main.css';
+import Router from './components/App';
+
 render(<Router />, document.querySelector('#main'));
